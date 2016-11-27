ea6001b2a518e81c4330d62f23e5ab59a9079fee
Andrii Halabuda <losAngelesBoy11@gmail.com>
2016-11-27 21:39:34 +0100

    Basic Sign In layout completed

---
+++ SocialNet/RoundedButton.swift
@@ -0,0 +1,18 @@
+//
+//  RoundedButton.swift
+//  SocialNet
+//
+//  Created by Andrew Foster on 11/27/16.
+//  Copyright © 2016 Andrii Halabuda. All rights reserved.
+//
+
+import UIKit
+
+class RoundedButton: UIButton {
+
+    override func awakeFromNib() {
+        
+        layer.cornerRadius = 5.0
+    }
+
+}
